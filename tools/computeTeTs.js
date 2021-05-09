2028f2ffa1d9d26a0b4a7b1f5d8cc2fee25809e4
Martin Ždila <m.zdila@gmail.com>
2021-05-09 21:49:22 +0200

    Create computeTeTs.js

---
+++ tools/computeTeTs.js
@@ -0,0 +1,18 @@
+const earthRadius = 6378137;
+
+const circ = earthRadius * Math.PI * 2;
+
+const zoom = process.argv[2];
+
+const pixels = Math.pow(2, zoom) * 256;
+
+const meterToPixel = circ / pixels;
+
+const bboxPixels = process.argv
+  .slice(3)
+  .map(m => Number(m))
+  .map(m => Math.round(m / meterToPixel));
+
+const bbox = bboxPixels.map(m => m * meterToPixel);
+
+console.log(`gdalwarp ... -te ${bbox.join(' ')} -ts ${bboxPixels[2] - bboxPixels[0]} ${bboxPixels[3] - bboxPixels[1]}`);
